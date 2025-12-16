f69fe0ee735301e5dd982740ec06ba1a670f4cb6
d <ddher101@gitlab.com>
2025-12-16 14:30:11 -0500

    Made the ble scanner part

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+
+
+	ble "github.com/NSM-Barii/yoda-ble/snatch"
+
+)
+
+
+
+
+func main(){
+
+	ble.Main()
+
+}
+
+
+
+
+// FIRST INSTALL 
+
+// go install golang.org/x/tools/gopls@latest
+
+
+
+
---
+++ snatch/ble_scanner.go
@@ -0,0 +1,95 @@
+package ble
+
+import (
+	"fmt"
+	"time"
+
+	"tinygo.org/x/bluetooth"
+)
+
+
+var adapter = bluetooth.DefaultAdapter
+
+type BLE struct {
+	Name string
+	Addr string
+	RSSI int16
+	UUID         []bluetooth.UUID
+	Manufacturer []bluetooth.ManufacturerDataElement
+}
+
+
+func BLE_Scanner() {
+	// This method will be responsible for performing BLE Scan
+
+	if err := adapter.Enable();  err != nil{
+		fmt.Println(err)
+	}
+
+
+	// LIST
+	devices := []BLE{}
+    
+
+	fmt.Println("[+] Launching BLE Scanner...")
+
+
+	err := adapter.Scan(func(a *bluetooth.Adapter, b bluetooth.ScanResult) {
+
+
+
+		name := b.LocalName()
+		addr := b.Address.String()
+		rssi := b.RSSI          
+		uuid := b.ServiceUUIDs()
+		manuf := b.ManufacturerData()
+
+        
+		fmt.Println("-----------")
+	    fmt.Println("Name: ", name)
+		fmt.Println("Addr: ", addr)
+		fmt.Println("RSSi: ", rssi)
+		fmt.Println("uuid: ", uuid)
+		fmt.Println("Manufacturer: ", manuf[0])
+
+
+
+
+		device := BLE{
+			Name: name,
+			Addr: addr,
+			RSSI: rssi,
+			UUID: uuid,
+			Manufacturer: manuf,
+
+		}
+
+
+		devices = append(devices, device)
+
+
+	})
+
+
+	if err != nil{
+		fmt.Println(err)
+	}
+
+
+
+}
+
+
+
+
+
+
+func Main(){
+	go BLE_Scanner()
+
+
+	<-time.After(5 * time.Second)
+	adapter.StopScan()
+	println("[-] Terminated BLE Scan!")
+	//println("Total Devices Found: ", len(BLE_Scanner().devices))
+}
